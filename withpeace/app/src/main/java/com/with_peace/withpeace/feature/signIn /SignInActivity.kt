0f379caf6da37b997024ad3ef62a36ed37f3d29a
rhkrwngud445 <rhkrwngud445@gmail.com>
2022-07-25 02:35:07 +0900

    :sparkles: 일반 로그인 화면 추가

---
+++ withpeace/app/src/main/java/com/with_peace/withpeace/feature/signIn /SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.with_peace.withpeace.feature.signIn
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.with_peace.withpeace.R
+
+class SignInActivity() : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
